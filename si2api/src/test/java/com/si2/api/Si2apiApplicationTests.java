4bd8ee930ccc4623c3b8b91f7bb070a2833727b6
Genilson <genisnilson@gmail.com>
2020-02-13 21:55:48 -0300

    Aula 1

---
+++ si2api/src/test/java/com/si2/api/Si2apiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.si2.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Si2apiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

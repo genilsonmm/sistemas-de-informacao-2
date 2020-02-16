e23ae1cc086a4e1d1b5fa4db7c784179783ee2c5
Genilson <genisnilson@gmail.com>
2020-02-16 10:30:27 -0300

    Aula2 - Postman e JSON

--- si2api/src/main/java/com/si2/api/Si2apiApplication.java
+++ si2api/src/main/java/com/si2/api/Si2apiApplication.java
@@ -2,11 +2,18 @@ package com.si2.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.si2.api.model.Usuario;
 
 @SpringBootApplication
 @RestController
@@ -29,5 +36,20 @@ public class Si2apiApplication {
 	@GetMapping("/usuario/id/{numero}")
 	public String user(@PathVariable String numero) {
 		return "usuário de id: " + numero;
+	}
+	
+	@PostMapping("/usuario")
+	public String cadastrarUsuario(@RequestBody Usuario usuario) {
+		return "Usuário " + usuario.getNome() + " cadastrado com sucesso!";
+	}	
+	
+	@DeleteMapping("/usuario/{id}")
+	public String deletarUsuario(@PathVariable int id) {
+		return "Usuário de Id: " + id + " removido com sucesso!";
+	}	
+	
+	@PutMapping("/usuario/{id}")
+	public String atualizarUsuario(@PathVariable int id, @RequestBody Usuario usuario) {
+		return "Usuário de Id: " + id + " atualizado para: " + usuario;
 	}
 }
---
+++ si2api/src/main/java/com/si2/api/model/Usuario.java
@@ -0,0 +1,31 @@
+package com.si2.api.model;
+
+public class Usuario {
+	
+	private int id;
+	private String nome;
+	private int idade;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public int getIdade() {
+		return idade;
+	}
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+	@Override
+	public String toString() {
+		return "Nome: " + nome + " | Idade: " + idade;
+	}
+}

e190e614c119adaaf7cf044faa6b1d1348a6c3fa
Genilson <genisnilson@gmail.com>
2020-04-05 21:42:18 -0300

    aplicativo livro

---
+++ frontend-app-livro/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="container">
+    
+    <Topo/>
+
+    <div class="row">
+      <MenuLivros :livros="livros"></MenuLivros> 
+      <Cadastro v-on:receiveBooksFromParent="addBook"></Cadastro>
+    </div>
+
+  </div>
+</template>
+
+<script>
+
+import Topo from './components/Topo.vue';
+import MenuLivros from './components/MenuLivros.vue';
+import Cadastro from './components/Cadastro.vue';
+
+export default {
+  name: 'App',
+  data(){
+    return {
+      livros: []
+    }
+  },
+  components: {
+    MenuLivros,
+    Topo,
+    Cadastro
+  },
+  methods: {
+    addBook(book){
+      this.livros.push(book);
+    }
+  }
+}
+</script>

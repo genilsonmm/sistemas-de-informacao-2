3bbe004d7ce0bde2d665d652299294f6adae6210
Genilson <genisnilson@gmail.com>
2020-11-17 07:03:09 -0300

    Exercícos vue

---
+++ 2020.2/app-comments/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <p id="title-text">App Comentários</p>
+    <Comments />
+  </div>
+</template>
+
+<script>
+
+import Comments from './components/Comments.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Comments
+  }
+}
+</script>
+
+<style>
+  #app{
+    font-family:Arial, Helvetica, sans-serif;
+  }
+
+  #title-text{
+    text-align: center;
+    font-size: 28px;
+  }
+</style>
---
+++ 2020.2/app-comments/src/components/Comments.vue
@@ -0,0 +1,47 @@
+<template>
+    <div>
+        <h1>Comentários</h1>
+        <div>
+            <p>
+                <input v-model="author" type="text" placeholder="Autor"/>
+            </p>
+            <p>
+                <textarea v-model="message" placeholder="Comentário"></textarea>
+            </p>
+            <button v-on:click="addComemnt">Comentar</button>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Comments',
+        data(){
+            return {
+                comments: [],
+                author: '',
+                message:''
+            }
+        },
+        methods: {
+            addComemnt(){
+                if(this.message.trim() === ''){
+                    return
+                }
+
+                let comment = {
+                    author: this.author,
+                    message: this.message
+                }
+
+                this.comments.push(comment);
+
+                console.log(this.comments)
+            }   
+        }
+    }
+</script>
+
+<style>
+
+</style>
